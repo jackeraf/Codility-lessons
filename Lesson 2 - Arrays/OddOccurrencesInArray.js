88d60f8ba6a6f7ec291f344c49ca869faf76c5dd
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-08-07 13:59:51 +0200

    oddoccurrences

--- Lesson 2 - Arrays/OddOccurrencesInArray.js
+++ Lesson 2 - Arrays/OddOccurrencesInArray.js
@@ -1,38 +1,39 @@
-// First 66% solution using a hashmap
+// First 100% solution using a hashmap
 // time O(N)
 
-// function solution(arr) {
-//   let nums = {};
-//   for (let i = 0; i < arr.length; i++) {
-//     const num = arr[i];
-
-//     if (!(num in nums)) {
-//       nums[num] = false;
-//     } else {
-//       nums[num] = true;
-//     }
-//   }
-//   for (let key in nums) {
-//     if (!nums[key]) return +key;
-//   }
-//   return 0;
-// }
+function solution(arr) {
+  let nums = {};
+  let max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    max = Math.max(num, max);
+    if (!(num in nums)) {
+      nums[num] = false;
+    } else {
+      nums[num] = !nums[num];
+    }
+  }
+  for (let key in nums) {
+    if (!nums[key]) return +key;
+  }
+  return max;
+}
 
 // time O(n^2) space O(1)
 
-function solution(arr) {
-  let length = arr.length;
-  if (length === 1) {
-    return arr[0];
-  }
-  arr.sort((a, b) => a - b);
-  for (let i = 0; i < length - 1; i += 2) {
-    if (arr[i] != arr[i + 1]) {
-      return arr[i];
-    }
-  }
-  return arr[length - 1];
-}
+// function solution(arr) {
+//   let length = arr.length;
+//   if (length === 1) {
+//     return arr[0];
+//   }
+//   arr.sort((a, b) => a - b);
+//   for (let i = 0; i < length - 1; i += 2) {
+//     if (arr[i] != arr[i + 1]) {
+//       return arr[i];
+//     }
+//   }
+//   return arr[length - 1];
+// }
 
 // Solution suggested with Exclusive operator (XOR) time O(n) space O(1) ?
 
