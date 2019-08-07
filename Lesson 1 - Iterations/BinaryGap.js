f1d6e28dc0b2861bf13137f8d917ccc39d603857
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-08-07 14:38:23 +0200

    binarygap

---
+++ Lesson 1 - Iterations/BinaryGap.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  let binary = (n >>> 0).toString(2);
+  let max = -Infinity;
+  let gapStart = false;
+  let zeroCounter = 0;
+
+  for (let i = 0; i < binary.length; i++) {
+    const number = +binary[i];
+    if (number === 1 && !gapStart) {
+      gapStart = true;
+    } else if (number === 1 && gapStart) {
+      max = Math.max(max, zeroCounter);
+      zeroCounter = 0;
+    } else {
+      zeroCounter++;
+    }
+  }
+  return max === -Infinity ? 0 : max;
+}
