6c800e9c12ab5ffea0307a70211e18371cd44777
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-09-03 14:14:02 +0200

    passing cars

--- Lesson 5 - Prefix Sums/PassingCars.js
+++ Lesson 5 - Prefix Sums/PassingCars.js
@@ -1,7 +1,29 @@
-//
+// Time O(n^2) space O(1)
 
-function solution(arr){
+// function solution(arr){
+//     let passingCarCounter = 0;
 
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = i +1; j < arr.length; j++){
+//             const difference = arr[i] - arr[j];
+//             if(difference === -1) passingCarCounter++;
+//         }
+//     }
+//     return passingCarCounter;
+// }
+
+// Time O(n) space O(1)
+
+function solution(arr) {
+  if (arr.length === 1) return 0;
+  let totalPairs = 0;
+  let carsEast = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) carsEast++;
+    else totalPairs += carsEast;
+  }
+  return totalPairs > 1000000000 ? -1 : totalPairs;
 }
 
-solution();
+solution([0, 1, 0, 1, 1]);
