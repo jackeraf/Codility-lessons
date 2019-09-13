351460ffa3e3baa173ec4efff3a6b733ee980f55
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-09-13 14:40:35 +0200

    MaxProductOfThree greedy

--- Lesson 6 - Sorting/MaxProductOfThree.js
+++ Lesson 6 - Sorting/MaxProductOfThree.js
@@ -10,5 +10,45 @@ function solution(arr) {
   );
 }
 
+// time O(n)
+// space O(1)
+
+function solution(arr) {
+  if (arr.length < 3) return 0;
+  let highest = Math.max(arr[0], arr[1]);
+  let lowest = Math.min(arr[0], arr[1]);
+  let highestProductOf2 = arr[0] * arr[1];
+  let lowestProductOf2 = arr[0] * arr[1];
+  let highestProductOf3 = arr[0] * arr[1] * arr[2];
+
+  for (let i = 2; i < arr.length; i++) {
+    let current = arr[i];
+
+    highestProductOf3 = Math.max(
+      highestProductOf3,
+      current * highestProductOf2,
+      current * lowestProductOf2
+    );
+
+    highestProductOf2 = Math.max(
+      highestProductOf2,
+      current * highest,
+      current * lowest
+    );
+
+    lowestProductOf2 = Math.min(
+      lowestProductOf2,
+      current * highest,
+      current * lowest
+    );
+
+    highest = Math.max(highest, current);
+
+    lowest = Math.min(lowest, current);
+  }
+
+  return highestProductOf3;
+}
+
 // solution([-3,1,2,-2,5,6]);
 solution([-5, 5, -5, 4]); // 125
