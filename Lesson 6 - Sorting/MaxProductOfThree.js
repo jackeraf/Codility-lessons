5d935c7e4ca16ce40151c5303f03874b9dbf34ea
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-09-13 14:11:34 +0200

    MaxProductOfThree

---
+++ Lesson 6 - Sorting/MaxProductOfThree.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+  if (arr.length < 3) return 0;
+  arr.sort((a, b) => a - b);
+  return Math.max(
+    arr[0] * arr[1] * arr[arr.length - 1],
+    arr[arr.length - 3] * arr[arr.length - 2] * arr[arr.length - 1]
+  );
+}
+
+// solution([-3,1,2,-2,5,6]);
+solution([-5, 5, -5, 4]); // 125
